e0e96b18cfa406d77d2d0f69186fb6b6523e461f
Priyansu123Chowdhury <102274788+Priyansu123Chowdhury@users.noreply.github.com>
2024-08-16 16:35:24 +0530

    On process

--- Social/src/App.jsx
+++ Social/src/App.jsx
@@ -1,18 +1,23 @@
+import { useState } from "react";
 import "./App.css";
+import CreatePost from "./components/CreatePost";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import PostList from "./components/PostList";
 import Sidebar from "./components/Sidebar";
 
 function App() {
+  const [selectedTab, setSelectedTab] = useState("Home");
   return (
     <div className="app-container">
-      <Sidebar />
+      <Sidebar selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
       <div className="content">
         <Header></Header>
+        {selectedTab === "Home" ? <PostList /> : <CreatePost />}
+
         <Footer></Footer>
       </div>
     </div>
-
   );
 }
 
